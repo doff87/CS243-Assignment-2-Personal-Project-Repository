347f400b78aa2d7b50a582581abedf1c117fa91e
doff87 <s-david.offutt@lwtech.edu>
2025-10-16 15:05:37 -0700

    Add first function. Input validation functions added.

--- pythonUtilityTool.py
+++ pythonUtilityTool.py
@@ -1,21 +1,153 @@
 def measurementConverter():
-    userOption = input("Please enter the number that corresponds to the measurement type you would like to convert: \n1. Weight\n2. Volume\n3. Distance\n4. Length").strip()
+    imperialOrMetric = 0
     
-    match userOption:
-        case 1:
-            print("Weight Placeholder")
-        case 2:
-            print("Volume Placeholder")
-        case 3:
-            print("Distance Placeholder")
-        case 4:
-            print("Length Placeholder")
-        case _:
-            print("Invalid Choice")
+    while True:
+        user_input = input("Please enter the number that corresponds to the measurement type you would like to convert: \n1. Mass\n2. Volume\n3. Length\n4. Quit\n").strip()
+    
+        userOption = is_int(user_input)
+
+        if userOption is None:
+            print("Invalid input. Please enter a number 1-5.")
+            continue
+
+        match userOption:
+            case 1:
+                imperialOrMetric = is_int(input("Please enter 1 if converting from imperial to metric, 2 if converting from metric to imperial, or enter 3 to return to menu: "))
+                
+                if imperialOrMetric not in (1,2,3):
+                    print("Invalid choice. Please review options.")
+                    continue
+                
+                if imperialOrMetric == 1:
+                    while True:
+                        user_input= input(("Please enter number of ounces to convert: ").strip())
+
+                        numUnits = is_float(user_input)
+
+                        if numUnits is None:
+                            print("Invalid input. Please enter a float.")
+                            continue
+                        
+                        print(f"{numUnits} ounces = {numUnits * 28.35} grams")
+                        break
+
+                elif imperialOrMetric == 2:
+                    while True:
+                        user_input= input(("Please enter number of grams to convert: ").strip())
+
+                        numUnits = is_float(user_input)
+
+                        if numUnits is None:
+                            print("Invalid input. Please enter a float.")
+                            continue
+                        
+                        print(f"{numUnits} grams = {numUnits/28.35} ounces")
+                        break
+
+                elif imperialOrMetric == 3:
+                    break
+
+            case 2:
+                imperialOrMetric = is_int(input("Please enter 1 if converting from imperial to metric, 2 if converting from metric to imperial, or enter 3 to return to menu: "))
+                
+                if imperialOrMetric not in (1,2,3):
+                    print("Invalid choice. Please review options.")
+                    continue
+                
+                if imperialOrMetric == 1:
+                    while True:
+                        user_input= input(("Please enter number of fluid ounces to convert: ").strip())
+
+                        numUnits = is_float(user_input)
+
+                        if numUnits is None:
+                            print("Invalid input. Please enter a float.")
+                            continue
+                        
+                        print(f"{numUnits} fluid ounces = {numUnits * 29.57} mililiters")
+                        break
+
+                elif imperialOrMetric == 2:
+                    while True:
+                        user_input= input(("Please enter number of mililiters to convert: ").strip())
+
+                        numUnits = is_float(user_input)
+
+                        if numUnits is None:
+                            print("Invalid input. Please enter a float.")
+                            continue
+                        
+                        print(f"{numUnits} mililiters = {numUnits/29.57} fluid ounces")
+                        break
+
+                elif imperialOrMetric == 3:
+                    return
+                
+            case 3:
+                imperialOrMetric = is_int(input("Please enter 1 if converting from imperial to metric, 2 if converting from metric to imperial, or enter 3 to return to menu: "))
+                
+                if imperialOrMetric not in (1,2,3):
+                    print("Invalid choice. Please review options.")
+                    continue
+                
+                if imperialOrMetric == 1:
+                    while True:
+                        user_input= input(("Please enter number of inches to convert: ").strip())
+
+                        numUnits = is_float(user_input)
+
+                        if numUnits is None:
+                            print("Invalid input. Please enter a float.")
+                            continue
+                        
+                        print(f"{numUnits} inches = {numUnits * 2.54} centimeters")
+                        break
+
+                elif imperialOrMetric == 2:
+                    while True:
+                        user_input= input(("Please enter number of centimeters to convert: ").strip())
+
+                        numUnits = is_float(user_input)
+
+                        if numUnits is None:
+                            print("Invalid input. Please enter a float.")
+                            continue
+                        
+                        print(f"{numUnits} centimeters = {numUnits/2.54} inches")
+                        break
+
+                elif imperialOrMetric == 3:
+                    break
+
+            case 4:
+                return
+
+            case _:
+                print("Invalid Choice")
+
+def is_int(userInput):
+    try:
+        return int(userInput)
+    except ValueError:
+        return None
+    
+def is_float(userInput):
+    try:
+        return float(userInput)
+    except ValueError:
+        return None
 
 def main():
-    measurementConverter()
+    while True:
+        user_input = input("Please enter the number that corresponds to the function you would like to utilize: 1. Measurement Converter\n2. Clock\n3. Dice Roll").strip()
     
+        userOption = is_int(user_input)
+
+        match userOption:
+
+            case 1:
+                
+
 
 main()
         
